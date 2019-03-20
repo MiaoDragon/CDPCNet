8068c28db75b199f5fe9e04a1049ccfb5e20d757
MiaoDragon <innocentmyl@gmail.com>
2019-03-20 04:07:50 -0700

    disable open3d

--- dataset/complex_util.py
+++ dataset/complex_util.py
@@ -5,7 +5,7 @@ including functions for generating collision and no-collision data
 import math
 import numpy as np
 import klampt
-import open3d
+#import open3d
 from klampt import vis as klvis
 import time
 import pandas as pd
@@ -41,6 +41,7 @@ for geo in geos:
     geo_pcs[geo][1,:] -= mean_y
     geo_pcs[geo][2,:] -= mean_z
     geo_pcs[geo] = 10 * geo_pcs[geo]
+"""
 def visualize(pcs):
     # visualize point cloud list
     pcd_list = []
@@ -56,7 +57,7 @@ def visualize(pcs):
         vis.add_geometry(pcd)
     vis.run()
     vis.destroy_window()
-
+"""
 
 def make_mesh(pc):
     # given PyntCloud
@@ -237,7 +238,7 @@ def generate_no_collision(batch=1000, N=2800):
     no_collision_data_P2 = np.array(no_collision_data_P2)
     return no_collision_data_P1, no_collision_data_P2
 
-
+"""
 def test():
     P1, P2 = generate_one_collision()
     visualize([P1, P2])
@@ -262,3 +263,4 @@ def test():
 
 if __name__=='__main__':
     test()
+"""
