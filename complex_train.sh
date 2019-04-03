1591bd1d4b988b57a3ffda9e9e0347e65b19a1fd
MiaoDragon <innocentmyl@gmail.com>
2019-04-03 10:50:20 -0700

    disable generator

--- complex_train.sh
+++ complex_train.sh
@@ -1,7 +1,7 @@
 # generate data
-cd dataset
-python3 complex_generator.py --data_size 80000 --minibatch 1000 --num_point 2800 --save_path complex/
-cd ..
+#cd dataset
+#python3 complex_generator.py --data_size 80000 --minibatch 1000 --num_point 2800 --save_path complex/
+#cd ..
 
 python3 complex_train.py --data_size 80000 --data_minibatch 1000 --batch_size 32 --num_points 2800 \
 --workers 4 --num_epoch 10 --out_path output/ --model complex_simple_model --seed 1 --num_workers 4 \
