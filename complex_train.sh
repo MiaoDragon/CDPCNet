cb42c2911eaa623c67680969c09f4c2a808de8d4
MiaoDragon <innocentmyl@gmail.com>
2019-04-03 16:56:39 -0700

    edit complex train

--- complex_train.sh
+++ complex_train.sh
@@ -3,10 +3,10 @@
 #python3 complex_generator.py --data_size 80000 --minibatch 1000 --num_point 2800 --save_path complex/
 #cd ..
 
-python3 complex_train.py --data_size 80000 --data_minibatch 1000 --batch_size 32 --num_points 2800 \
---workers 4 --num_epoch 10 --out_path output/ --model complex_simple_model --seed 1 --num_workers 4 \
---pin_memory 1 --val_batch 10 --save_epoch 2 --model_type simple
+#python3 complex_train.py --data_size 80000 --data_minibatch 1000 --batch_size 32 --num_points 2800 \
+#--workers 4 --num_epoch 10 --out_path output/ --model complex_simple_model --seed 1 --num_workers 4 \
+#--pin_memory 1 --val_batch 10 --save_epoch 2 --model_type simple --data_path /media/arclabdl1/HD1/CDPCNet/complex/
 
 python3 complex_train.py --data_size 80000 --data_minibatch 1000 --batch_size 32 --num_points 2800 \
 --workers 4 --num_epoch 10 --out_path output/ --model complex_deep_model --seed 1 --num_workers 4 \
---pin_memory 1 --val_batch 10 --save_epoch 2 --model_type deep
+--pin_memory 1 --val_batch 10 --save_epoch 2 --model_type deep --data_path /media/arclabdl1/HD1/CDPCNet/complex/
