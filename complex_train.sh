63ca2a21c5acb7265d1af13122ced6543e8b95f1
MiaoDragon <innocentmyl@gmail.com>
2019-04-03 17:07:08 -0700

    path wrong

--- complex_train.sh
+++ complex_train.sh
@@ -9,4 +9,4 @@
 
 python3 complex_train.py --data_size 80000 --data_minibatch 1000 --batch_size 32 --num_points 2800 \
 --workers 4 --num_epoch 10 --out_path output/ --model complex_deep_model --seed 1 --num_workers 4 \
---pin_memory 1 --val_batch 10 --save_epoch 2 --model_type deep --data_path /media/arclabdl1/HD1/CDPCNet/complex/
+--pin_memory 1 --val_batch 10 --save_epoch 2 --model_type deep --data_path /media/arclabdl1/HD1/YLmiao/CDPCNet/complex/
