248eb4e0c09d2e2f332e8e911deedb8c62c542fc
MiaoDragon <innocentmyl@gmail.com>
2019-04-15 12:29:07 -0700

    no save

--- complex_test.sh
+++ complex_test.sh
@@ -5,4 +5,4 @@
 
 python3 complex_test.py --data_size 80000 --data_minibatch 1000 --batch_size 32 --num_points 2800 \
 --workers 4 --num_epoch 10 --out_path output/ --model complex_simple_model --seed 1 --num_workers 4 \
---pin_memory 1 --val_batch 10 --save_epoch 10 --model_type simple --data_path /media/arclabdl1/HD1/YLmiao/CDPCNet/complex/
+--pin_memory 1 --val_batch 10 --model_type simple --data_path /media/arclabdl1/HD1/YLmiao/CDPCNet/complex/
