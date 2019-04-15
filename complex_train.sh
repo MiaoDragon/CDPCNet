9f694427bb93b0681ce70f930d755d5f3d2a44ec
MiaoDragon <innocentmyl@gmail.com>
2019-04-15 12:44:11 -0700

    debug

--- complex_train.sh
+++ complex_train.sh
@@ -10,4 +10,4 @@ cd ..
 python3 complex_train.py --data_size 80000 --data_minibatch 1000 --batch_size 32 --num_points 2800 \
 --workers 4 --num_epoch 10 --out_path output/ --model complex_simple_model --seed 1 --num_workers 4 \
 --pin_memory 1 --val_batch 10 --save_epoch 2 --model_type simple --data_path /media/arclabdl1/HD1/YLmiao/CDPCNet/complex/ \
---start_epoch 5
+--start_epoch 0
